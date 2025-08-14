756173bc40bea65b6d3922801b29e7e2d0b8e89f
Gabriel Patano Kiyot <gabrielkiyoto@unifev.corp.br>
2025-08-14 16:54:47 -0300

    função inserir adicionada

--- tarefa.controller.php
+++ tarefa.controller.php
@@ -1,3 +1,18 @@
 <?php
-    print_r($_POST)
+    require "tarefa.model.php";
+    require "tarefa.service.php";
+    require "conexao.php";
+
+    print_r($_POST);
+
+    $tarefa = new Tarefa();
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->inserir();
+
+    print_r($tarefaService);
+
 ?>
